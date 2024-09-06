fdcbd477ecdbef2172584337ebe6860dba32fe0a
Bielzinn <108942711+Bielzinmilgrau@users.noreply.github.com>
2024-09-06 15:45:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,53 @@
-const botoes  = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".abas-conteudo");
-const tempo = document.querySelectorAll(".contador");
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
 
-const tempoObjetivo1 = new Date(2030,11,24,23,59);
-const tempoObjetivo2 = new Date(2030,9,28,23,59);
-const tempoObjetivo3 = new Date(2030,0,1,23,59);
-const tempoObjetivo4 = new Date(2023,2,14,23,59);
-
+const  tempoObjetivo1 = new Date(2024,11,30);
+const  tempoObjetivo2 = new Date(2024,11,30);
+const  tempos = [tempoObjetivo1,tempoObjetivo2];
 function calculaTempo(tempoObjetivo){
-const agora = new Date();
-let segundos 
-let minutos
-let horas
-let dias
-segundos = Math.floor((tempoObjetivo-agora)/1000)
-minutos = Math.floor(segundos/60)
-horas = Math.floor(minutos/60)
-dias = Math.floor(horas/24)
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dias
+    segundos = ((tempoObjetivo-agora)/1000)
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos);
+    minutos = Math.floor(minutos);
+    horas = Math.floor(horas)
+    dias = Math.floor(dias)
 
-segundos = segundos%60
-minutos = minutos%60
-horas = horas%24
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
 
-if(segundos >=0){
-   return [dias,horas,minutos,segundos]
-} else{
-    return 'Prazo finalizado!'
- }
+    if(segundos >= 0){
+    return `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+    } else {  
+        return 'Prazo Finalizado!'   
+    }
+
 }
-const tempoUm = calculaTempo(tempoObjetivo1)
-const tempoDois = calculaTempo(tempoObjetivo2)
-const tempoTres = calculaTempo(tempoObjetivo3)
-const tempoQuatro = calculaTempo(tempoObjetivo4)
+function atualizaCronometro(){
 
-console.log(tempoUm[3])
+    for (let i=0; i<contador.length;i++){
+        contador[i]. textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+comecaCronometro()
 
-tempo[0].textContent = calculaTempo(tempoObjetivo1);
-tempo[1].textContent = calculaTempo(tempoObjetivo2);
-tempo[2].textContent = calculaTempo(tempoObjetivo3);
-tempo[3].textContent = calculaTempo(tempoObjetivo4);
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function(){
-        for (let j=0;j<botoes.length;j++){
+for(let i = 0; i < botoes.length ; i++){
+    botoes[i].onclick = function() {
+        for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
